2552ff5144db7a236c5f39eced7114160a7a9ec3
Vincent Walker <vinnywalker920@gmail.com>
2024-02-05 19:06:07 -0600

    Fixed and Working Dev

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 !# /bin/bash
-#Installs All Relevant Packages
+#Updates Mirrors and Installs All Relevant Packages
+sudo pacman -Syy
 sudo pacman -S neofetch vscode sed htop qemu-full virt-manager virt-viewer dnsmasq bridge-utils libguestfs ebtables vde2 openbsd-netcat lightdm lightdm-gtk-greeter lightdm-slick-greeter
 
 #Does Some Magic So KVM Works
@@ -11,13 +12,16 @@ sudo usermod -aG libvirt $USER
 sudo systemctl start libvirtd.service
 sudo systemctl enable libvirtd.service
 
-#Configures Greeter and Customizes it
+#Changes LightDM Config to Allow New Greeter
 sudo sed -i 's/#greeter-session=example-gtk-gnome/greeter-session=lightdm-gtk-greeter/' /etc/lightdm/lightdm.conf
 sudo sed -i 's/#logind-check-graphical=true/logind-check-graphical=true/' /etc/lightdm/lightdm.conf
-#sudo sed -i 's/Session=default/Session=hyprland' ~/.dmrc
+
+#Copies Greeter Background to the Right Place
 sudo cp ~/Downloads/EndeavourOS_Schematic_Install/wallpaper/midnight.jpg /usr/share/pixmaps/
-#sudo echo "[Greeter]'\n'background=/usr/share/pixmaps/midnight.jpg" >> /etc/lightdm/slick-greeter.conf
+
+#Sets Greeter Background
 sudo sed -i 'sX#background=Xbackground=/usr/share/pixmaps/midnight.jpgX' /etc/lightdm/lightdm-gtk-greeter.conf
+
 #Enables LightDM Which Will Start Greeter Upon Reboot
 sudo systemctl enable lightdm.service
 
